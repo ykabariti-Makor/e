1ba893507da6044eefb803a393e877f8095334c5
theExperienced <95164291+ykabariti-Makor@users.noreply.github.com>
2022-02-07 16:14:22 +0200

    config updated

--- config.js
+++ config.js
@@ -28,8 +28,8 @@ const config = {
     symbol: '#?!@$%^&*-',
   },
   URL: {
-    pathIncluded: true,
     domainOnly: false,
+    pathIncluded: true,
   },
   phones: {},
   tags: {
@@ -61,8 +61,8 @@ const setConfig = (configName, parameters) => {
       config.numsFormater.decimalDigitLimit = parameters.decimalDigitLimit
       break
     case 'url' || 'URL':
-      config.URL.domainOnly = parameters.domainOnly
-      config.URL.pathIncluded = parameters.pathIncluded
+      if (parameters.domainOnly) config.URL.domainOnly = parameters.domainOnly
+      if (parameters.pathIncluded) config.URL.pathIncluded = parameters.pathIncluded
       break
     case 'tags':
       config.tags.specialChars = parameters.specialChars
