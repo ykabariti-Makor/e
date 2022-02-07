887bf3e3899cd887ef356e4d5672a3907ff234d1
theExperienced <95164291+ykabariti-Makor@users.noreply.github.com>
2022-02-07 15:46:42 +0200

    config updated

--- config.js
+++ config.js
@@ -60,7 +60,7 @@ const setConfig = (configName, parameters) => {
       config.numsFormater.overallDigitLimit = parameters.overallDigitLimit
       config.numsFormater.decimalDigitLimit = parameters.decimalDigitLimit
       break
-    case 'url':
+    case 'url' || 'URL':
       config.URL.domainOnly = parameters.domainOnly
       config.URL.pathIncluded = parameters.pathIncluded
       break
