f916e2573ceb6abf2c6c67de56d68d3586a22474
theExperienced <95164291+ykabariti-Makor@users.noreply.github.com>
2022-02-08 13:18:16 +0200

    merged completed

--- utils/modifies.js
+++ utils/modifies.js
@@ -92,15 +92,15 @@ const tagsSeparator = (string, separators) => {
   }
 }
 
+/**
+ * Number formatter for numbers
+ * @param {numToFormat} string
+ * @returns string
+ */
 const magnitudeUnits = {
   1: 'K',
   2: 'M',
   3: 'G',
-}
-
-const isValidStringedNumber = (numToFormat) => {
-  if (typeof numToFormat != 'string') return false
-  return !isNaN(numToFormat) && !isNaN(parseFloat(numToFormat))
 }
 
 /**
@@ -112,7 +112,7 @@ const numFormatter = (numToFormat) => {
   if (!isValidStringedNumber(numToFormat))
     return {
       success: false,
-      message: 'Is not a valid number',
+      message: '',
     }
 
   const { overallDigitLimit, decimalDigitLimit } = config.numsFormater
@@ -130,110 +130,58 @@ const numFormatter = (numToFormat) => {
     //the processed number is the new number that has been handled + the letter represent the thousends sliced
     processedNumber = overallHandlement.num
     unitSuffix = overallHandlement.unitSuffix
-    const magnitudeUnits = {
-      1: 'K',
-      2: 'M',
-      3: 'G',
-    }
-
-    const numFormatter = (numToFormat, { overallDigitLimit = 10, decimalDigitLimit = 2 }) => {
-      const isFloatingPoint = String(numToFormat).includes('.') ? 1 : 0
-      let processedNumber
-      //!floating point cutting
-      if (isFloatingPoint) processedNumber = decimalHandler(numToFormat, decimalDigitLimit)
-
-      processedNumber = String(processedNumber)
-
-      let unitSuffix
-      if (processedNumber.length - isFloatingPoint > overallDigitLimit) {
-        //!main numer formatting
-        overallHandlement = overallHandler(processedNumber, overallDigitLimit, isFloatingPoint)
-
-        processedNumber = overallHandlement.num
-        unitSuffix = overallHandlement.unitSuffix
-      }
-
-      //seperate the number by the floating point
-
-      const [left, right] = processedNumber.split('.')
-
-      //returns the handled number seperated by commas, attach the right side if exist and append the letter representing the thousends sliced
-
-      return {
-        success: true,
-        message: 'Successfully formatted number',
-        data: left.replace(/\B(?=(\d{3})+(?!\d))/g, ',') + (right ? '.' + right : '') + (unitSuffix ?? ''),
-      }
-    }
-
-    // handle the overall digit
-    // const overallHandler = (num, limit, isFloatingPoint) => {
-    //   //if the number contains floating point and the number is over the limit start to slice away the numbers
-    //   // until it meets the limit or until reaching the floating point
-    //   if (isFloatingPoint)
-    //     while (num.length - isFloatingPoint > limit) {
-    //       num = num.substring(0, num.length - 1)
-
-    //       if (num.length - isFloatingPoint === limit) {
-    //         return { num }
-    //       }
-
-    //       if (num.charAt(num.length - 1) === '.') {
-    //         num = num.substring(0, num.length - 1)
-    //         break
-    //       }
-    //     }
-    //   //thousend sliced counter initialized with 0
-    //   let thousandsSliced = 0,
-    //     remainder
-    //   //the number exceeds the limit start slicing away by thousend at each time, save the sliced digits aside and count the thousends sliced
-    //   //!adding commas
-
-    //   const [left, right] = processedNumber.split('.')
-
-    //   return left.replace(/\B(?=(\d{3})+(?!\d))/g, ',') + (right ? '.' + right : '') + (unitSuffix ?? '')
-    // }
-
-    //////////////////! DECIMAL LIMITER
-    const decimalHandler = (numToFormat, decimalDigitLimit) => {
-      //!DONE WITH
-      return numToFormat.toFixed(decimalDigitLimit)
-    }
-
-    //////////////////! OVERALL LIMITER - MATH BASED
-    const overallHandler = (num, limit, isFloatingPoint) => {
-      //!slice away right of point
-      while (num.length - isFloatingPoint > limit) {
-        num = num.substring(0, num.length - 1)
-
-        if (num.length - isFloatingPoint === limit) {
-          return { num } //!probably need to get rid of floating point
-        }
-        if (num.charAt(num.length - 1) === '.') {
-          num = num.substring(0, num.length - 1)
-          break
-        }
-      }
-
-      // //!check if meets requirement - then return
-
-      let thousandsSliced = 0,
-        remainder
-      //!keep slicing away left of point - by block of 3's
-      while (num.length > limit) {
-        remainder = num.substring(num.length - 3)
-        num = num.substring(0, num.length - 3)
-
-        thousandsSliced++
-      }
-      //return the number + floating point if needed + the chunk from the remainder the meets the limit
-      //also return the letter that represent the number of thousends sliced
-      return {
-        num: num + (limit - num.length ? '.' : '') + remainder.substring(0, limit - num.length),
-        unitSuffix: magnitudeUnits[thousandsSliced],
-      }
-    }
   }
+
+  //seperate the number by the floating point
+
+  const [left, right] = processedNumber.split('.')
+
+  //returns the handled number seperated by commas, attach the right side if exist and append the letter representing the thousends sliced
+
+  return {
+    success: true,
+    message: 'Successfully formatted number',
+    data: left.replace(/\B(?=(\d{3})+(?!\d))/g, ',') + (right ? '.' + right : '') + (unitSuffix ?? ''),
+  }
+}
+
+// handle the overall digit
+const overallHandler = (num, limit, isFloatingPoint) => {
+  //if the number contains floating point and the number is over the limit start to slice away the numbers
+  // until it meets the limit or until reaching the floating point
+  if (isFloatingPoint)
+    while (num.length - isFloatingPoint > limit) {
+      num = num.substring(0, num.length - 1)
+
+      if (num.length - isFloatingPoint === limit) {
+        return { num }
+      }
+
+      if (num.charAt(num.length - 1) === '.') {
+        num = num.substring(0, num.length - 1)
+        break
+      }
+    }
+  //thousend sliced counter initialized with 0
+  let thousandsSliced = 0,
+    remainder
+  //the number exceeds the limit start slicing away by thousend at each time, save the sliced digits aside and count the thousends sliced
+  while (num.length > limit) {
+    remainder = num.substring(num.length - 3)
+    num = num.substring(0, num.length - 3)
+
+    thousandsSliced++
+  }
+  //return the number + floating point if needed + the chunk from the remainder the meets the limit
+  //also return the letter that represent the number of thousends sliced
+  return {
+    num: num + (limit - num.length ? '.' : '') + remainder.substring(0, limit - num.length),
+    unitSuffix: magnitudeUnits[thousandsSliced],
+  }
+}
+const isValidStringedNumber = (numToFormat) => {
+  if (typeof numToFormat != 'string') return false
+  return !isNaN(numToFormat) && !isNaN(parseFloat(numToFormat))
 }
 
 /**
