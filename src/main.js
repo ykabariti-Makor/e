49a5de2ba55e9a179d2f92a807abafec214d6e71
theExperienced <95164291+ykabariti-Makor@users.noreply.github.com>
2022-02-09 15:51:20 +0200

    email_domain

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const { setConfig } = require('./config');
 const { tagsSeparator, numberFormatter, phoneNumberFormatter, specialCharsModifier, removeSpaces } = require('./functions/modifies');
-const { URLValidator, passwordValidation, ValidateIPaddress, checkNumberPositivity, emailDomainValidator} = require('./functions/auth');
+const { URLValidator, passwordValidation, ValidateIPaddress, checkNumberPositivity, emailDomainValidator } = require('./functions/auth');
 
 module.exports = {
 	setConfig,
