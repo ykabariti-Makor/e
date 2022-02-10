90a56ee5b2f76dc980d305bd4b4bb70b1b0b36dd
Daniel <daniel@gmail.com>
2022-02-10 11:45:17 +0200

    added babel for ES6 syntax

--- src/functions/modifies.js
+++ src/functions/modifies.js
@@ -1,4 +1,5 @@
-const { config } = require('../config');
+// const { config } = require('../config');
+import { config } from '../config';
 const { overallHandler } = require('../utils/modifies');
 
 /**
